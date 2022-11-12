5c147304218f9b3a7bc7b4830a4a93051fa92886
mihneapopesq <76889236+mihneapopesq@users.noreply.github.com>
2022-11-12 22:49:39 +0200

    Add files via upload

---
+++ scary maze game/scaryMaze/scaryMaze/Form1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace scaryMaze
+{
+    public partial class Form1 : Form
+    {
+
+        System.Media.SoundPlayer player = new System.Media.SoundPlayer();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void endGame(object sender, EventArgs e)
+        {
+            reset();
+        }
+
+        private void start(object sender, EventArgs e)
+        {
+            button1.Enabled = false;
+            StarGame();
+        }
+
+        private void ScarePlayer(object sender, EventArgs e)
+        {
+            Scare fullscreen = new Scare();
+            fullscreen.Show();
+        }
+
+        private void StarGame()
+        {
+            foreach(Control x in this.Controls)
+            {
+                if(x is PictureBox)
+                {
+                    x.Enabled = true;
+                    x.BackColor = System.Drawing.Color.SkyBlue;
+                }
+            }
+
+            player.Stream = Properties.Resources.bg_music;
+            player.PlayLooping();
+        }
+        private void reset()
+        {
+            button1.Enabled = true;
+            player.Stop();
+
+            foreach(Control x in this.Controls)
+            {
+                if(x is PictureBox)
+                {
+                    x.Enabled = false;
+                    x.BackColor = System.Drawing.Color.Black;
+                }
+            }
+        }
+    }
+}
---
+++ scary maze game/scaryMaze/scaryMaze/Scare.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace scaryMaze
+{
+    public partial class Scare : Form
+    {
+        public Scare()
+        {
+            InitializeComponent();
+
+            //make this form full screen
+            this.WindowState = FormWindowState.Normal;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Bounds = Screen.PrimaryScreen.Bounds;
+
+            //play the scart sound
+            System.Media.SoundPlayer player = new System.Media.SoundPlayer();
+            player.Stream = Properties.Resources.scare;
+            player.Play();//play this once
+        }
+
+        private void exit(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
